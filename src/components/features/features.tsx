41d96752bd7b02bbe6d38069854ef05516f7f757
Samuel <samuel.corrales621@gmail.com>
2026-01-13 21:14:11 -0700

    fix responsive

--- src/components/features/features.tsx
+++ src/components/features/features.tsx
@@ -6,14 +6,16 @@ import { ZonesCard } from './zones-card/zones-card';
 
 export const Featured = () => {
   return (
-    <section className="max-w-5xl m-auto flex flex-col gap-10 px-6">
-      <SectionTitle headline="Simple by design" title="Powerful by default" />
+    <section className="px-6">
+      <div className="max-w-5xl m-auto flex flex-col gap-10">
+        <SectionTitle headline="Simple by design" title="Powerful by default" />
 
-      <div className="grid grid-cols-1 lg:grid-cols-8 gap-8">
-        <FulfillmentCard />
-        <VariantsCard />
-        <CustomFieldsCard />
-        <ZonesCard />
+        <div className="grid grid-cols-1 lg:grid-cols-8 gap-8">
+          <FulfillmentCard />
+          <VariantsCard />
+          <CustomFieldsCard />
+          <ZonesCard />
+        </div>
       </div>
     </section>
   );
